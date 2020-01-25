5ee41104eb159ec7af87f9e5fdc79c8aa6d93090
Bijaya Limbu <bl18aat@herts.ac.uk>
2020-01-25 10:16:09 +0000

    editing page with css

---
+++ src/components/Skills.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <div class="holder">
+      <ul>
+        <ul v-for="(data, index) in skills" :key='index'>{{ index }}. {{ data.skill }}</ul>
+      </ul>
+    <div v-bind:class="{ alert: showAlert}"></div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data() {
+    return {
+      skills: [
+        {"skill": "Vue.js"},
+        {"skill": "Frontend Developer"},
+      ],
+      showAlert: true
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.alert {
+  background-color: yellow;
+  width: 100%;
+  height: 30px;
+}
+
+</style>
