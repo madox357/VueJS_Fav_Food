9888198124ffc6c9eac988f294317b9a0e52656e
Bijaya Limbu <bl18aat@herts.ac.uk>
2020-01-25 20:06:46 +0000

    got submit to return data

--- src/App.vue
+++ src/App.vue
@@ -17,9 +17,21 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+body {
+  background-color: #EEEEEE;
+  font-family: 'Montserrat', sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  align-items: center;
+}
+body, html {
+  margin: 0;
+  height: 100%;
+}
 #app {
-  
-  text-align: center;
- 
+    width: 50%;
 }
 </style>
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="hello">
     <div class="holder">
+      <form @submit.prevent="addSkill">
+        <input type="text" placeholder="Enter the name of favorite food." v-model="skill"  >
+        
+      </form>
+     
       <ul>
-        <ul v-for="(data, index) in skills" :key='index'>{{ index }}. {{ data.skill }}</ul>
+        <li v-for="(data, index) in skills" :key='index'>{{ data.skill }}</li>
       </ul>
-    <div v-bind:class="{ alert: showAlert}"></div>
+    <p>My Favorite food.</p>
     </div>
-
   </div>
 </template>
 
@@ -15,12 +19,18 @@ export default {
   name: 'Skills',
   data() {
     return {
+      
+      skill: '',
       skills: [
-        {"skill": "Vue.js"},
-        {"skill": "Frontend Developer"},
-      ],
-      showAlert: true
-
+        {"skill": "Pizza"},
+        {"skill": "Dougnut"}
+      ]      
+    }
+  },
+  methods: {
+    addSkill() {
+      this.skills.push({skill: this.skill})
+      this.skill = '';
     }
   }
 }
@@ -28,10 +38,44 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.alert {
-  background-color: yellow;
-  width: 100%;
-  height: 30px;
-}
+
+  .holder {
+    background: #fff;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
+
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+  }
+
 
 </style>
